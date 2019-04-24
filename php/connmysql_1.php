3aca23107cddb4f55082db49bb59a65b50be41f0
ddindu <49517659+ddindu@users.noreply.github.com>
2019-04-24 11:47:22 +0900

    Add files via upload

--- php/connmysql_1.php
+++ php/connmysql_1.php
@@ -2,21 +2,21 @@
 <head> <meta charset="utf-8"/> </head>
 <?php
     
-    $host = "localhost";
-    $user = 'root';
-    $pw = "apmsetup";
-    $dbName = 'sqldb';
+    $host="localhost";
+    $user='root';
+    $pw="apmsetup";
+    $dbName='sqldb';
 
-    $mysql = mysql_connect($host,$user,$pw);
-    mysql_select_db($dbName,$mysql);
-    $sql = "insert into 구매테이블(num,userID,prodName,category,price,amount) 
-    value(13,kjs,컴퓨터,전자,100,1)";
-    $result=mysql_query($sql);
+    $mysql=mysqli_connect($host,$user,$pw,$dbName);
+
+    $sql="insert into 구매테이블(num,userID,prodName,category,price,amount)"; 
+    $sql.="value('13','asd','0102','kse','144','155')";
+    $result=mysqli_query($mysql,$sql);
     if($result){
         echo "레코드 삽입 성공";
     }else{
         echo "레코드 삽입 실패";
     }
-    mysql_close($mysql) // 접속끊기
+    mysqli_close($mysql); // 접속끊기
 ?>
 </html>
