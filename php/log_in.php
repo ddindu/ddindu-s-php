dc5aed626ba51b8171f438513d4463a12ec6374d
ddindu <49517659+ddindu@users.noreply.github.com>
2019-05-22 11:13:03 +0900

    Delete log_in.php

--- php/log_in.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if($_POST['id']=='server'){
-        if($_POST['pw']=='1234'){
-            echo 'Login succeed';
-        }else { echo 'password wrong';}
-    }else { echo 'ID wrong ';}
-?>
