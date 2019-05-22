952f8f5e4b4eed08c82b44d2dc759c2fc78fce9a
ddindu <49517659+ddindu@users.noreply.github.com>
2019-05-22 11:08:09 +0900

    Add files via upload

--- php/himdle/delete.php
+++ php/himdle/delete.php
@@ -4,5 +4,5 @@
     mysqli_set_charset($link,"utf8");
     $sql = "DELETE FROM humaninfo WHERE Age = $Age";
     $result = mysqli_query($link, $sql);
-    echo ("<script>location.href='/php/himdle/HumanInfo2.php';</script>");
+    echo ("<script>location.href='./HumanInfo2.php';</script>");
 ?>
